57ccbb9042317676508691de51022f38c2ee91f3
Ubuntu <jenyoolumide@yahoo.com>
2023-06-25 15:06:56 +0000

    clint configuration file using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Modifying configuration file
+
+augeas { 'Turn off passwd auth':
+  context => '/etc/ssh/ssh_config',
+  changes => [
+    'set PasswordAuthentication no',
+  ],
+}
+
+augeas { 'Declare identity file':
+  context => '/etc/ssh/ssh_config',
+  changes => [
+    'set IdentityFile ~/.ssh/school',
+  ],
+}
