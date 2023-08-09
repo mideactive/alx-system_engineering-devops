45118e483a25fd0ec73abe90ee93d19bba7b94b5
mideactive <jenyoolumide@yahoo.com>
2023-08-09 20:18:35 +0000

    fix server error 500

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,22 +1,6 @@
-# puppet code
-class web_stack_debugging {
-  # Install Apache2 package
-  package { 'apache2':
-    ensure => installed,
-  }
+# Fix 500 error
 
-  # Fix Apache virtual host configuration
-  file { '/etc/apache2/sites-available/default.conf':
-    ensure  => present,
-    content => "# Updated Apache virtual host configuration\n# ...",
-    require => Package['apache2'],  # Requires the apache2 package to be installed
-    notify  => Service['apache2'],
-  }
-
-  # Restart Apache service after the configuration change
-  service { 'apache2':
-    ensure  => running,
-    enable  => true,
-    require => Package['apache2'],  # Requires the apache2 package to be installed
-  }
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
